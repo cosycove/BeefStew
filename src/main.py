853658f4e7f3f4d3a7172e8a2baba79fc4feb40b
subatomicseagull <subatomicseagull@gmail.com>
2026-05-15 12:45:54 +0100

    env checker

--- src/main.py
+++ src/main.py
@@ -22,11 +22,15 @@ except Exception:
 log_level = os.getenv("LOG_LEVEL", "WARNING").upper()
 logging.basicConfig(level=log_level, format="[%(levelname)s] %(asctime)s - %(message)s")
 
+# verify the environemnt variables
+environment_check()
 
 sp_client = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials(client_id=os.getenv("SPOTIFYCLIENTID"),client_secret=os.getenv("SPOTIFYCLIENTSECRET"),))
+# add a check to see if can the API endpoint be reached?
 
 trello_client = TrelloClient(
 api_key=os.getenv("TRELLOAPIKEY"),api_secret=os.getenv("TRELLOAPISECRET"),token=os.getenv("TRELLOTOKEN"),)
+# add a check to see if can the API endpoint be reached?
 
 bot_loop: asyncio.AbstractEventLoop | None = None
 
@@ -91,63 +95,94 @@ async def _guild_only_interaction_check(interaction: discord.Interaction) -> boo
 bot.tree.interaction_check = _guild_only_interaction_check
 
 
-async def environment_check():
-    
-    # check each env var, log a warning if its missing, and critial if the token or db is missing.
-    
+def environment_check(): 
     if not os.getenv("TOKEN"):
         logging.critical("Bot token is missing or invalid.")
         exit()
         
-        # check if guild ID matches the current server
+        # check if guild ID exists and matches the current server
         if bot.get_guild(int(os.getenv("GUILDID"))) != os.getenv("GUILDID"):
             logging.warning("Guild ID does not match the current guild, some features might not work.")
         
-        # check if the client ID matches the current bot application
+        # check if the client ID exists and matches the current bot application
         if bot.user.id != int(os.getenv("CLIENTID")):
             logging.warning("Client ID does not match the current bot application, some features might not work.")
 
-        # check if the postgres DB is reachable
-        try:
-            import psycopg2
-            connection = psycopg2.connect(
-                dbname=os.getenv("DBNAME"),
-                user=os.getenv("DBUSER"),
-                password=os.getenv("DBPASS"),
-                host=os.getenv("DBHOST"),
-                port=os.getenv("DBPORT")
-            )
-            connection.close()
-        except Exception as e:
-            logging.critical(f"Could not connect to PostgreSQL database: {e}")
-            exit()
-        
-        # check if the SMB server is reachable
-        from smb.SMBConnection import SMBConnection
-        smb = SMBConnection(
-            os.getenv("SMBUSER"), os.getenv("SMBPASS"), "local_client", os.getenv("SERVERIP"), use_ntlm_v2=True, is_direct_tcp=True)
-        if not smb.connect(server, 445):
-            logging.ERROR("Could not connect to SMB server, some features might not work.")
-        
+        # check if postgres creds exist
+        dbname=os.getenv("DBNAME")
+        if dbname == "" or dbname == None:
+            logging.error("Database name environemnt variable is missing, core features will not work.")
+        user=os.getenv("DBUSER")
+        if user == "" or user == None:
+            logging.error("Database username environemnt variable is missing, core features will not work.")
+        password=os.getenv("DBPASS")
+        if password == "" or password == None:
+            logging.error("Database password environemnt variable is missing, core features will not work.")
+        host=os.getenv("DBHOST")
+        if host == "" or host == None:
+            logging.error("Database hostname environemnt variable is missing, core features will not work.")
+        port=os.getenv("DBPORT")
+        if port == "" or port == None:
+            logging.error("Database port environemnt variable is missing, core features will not work.")
+
+        # check if SMB creds exist
+        user = os.getenv("SMBUSER")
+        if user == "" or user == None:
+            logging.warning("SMB username environemnt variable is missing, some features will not work.")
+        password = os.getenv("SMBPASS")
+        if password == "" or password == None:
+            logging.warning("SMB password environemnt variable is missing, some features will not work.")
+        host = os.getenv("SERVERIP")
+        if host == "" or host == None:
+            logging.warning("SMB server IP environemnt variable is missing, some features will not work.")
+
+        host = os.getenv("HOSTNAME")
+        if host == "" or host == None:
+            logging.warning("File server hostname environemnt variable is missing, some features will not work.")
+
         # check the spotify API credentials
-        
-        #check the trello API credentials & board ID
-        
-        #check the google API credentials
-        
-        #check the wiki API credentials
-        
-        # check to see if there are any TTS voices installed
-        
-        #check if any packages are out of date?
+        spclientid = os.getenv("SPOTIFYCLIENTID")
+        if spclientid == "" or spclientid == None:
+            logging.warning("Spotify API Client ID is missing, some VC features like the music player or downloader might not work.")
+        spsecret = os.getenv("SPOTIFYCLIENTSECRET")
+        if spsecret == "" or spsecret == None:
+            logging.warning("Spotify API Client Secret is missing, some VC features like the music player or downloader might not work.")
         
         # ytdl prerequisites check
         ffmpeg_path = os.getenv("FFMPEGEXE")
         if not ffmpeg_path or not os.path.exists(ffmpeg_path):
-            logging.warning("Could not find ffmpeg executable, some vc features like the music player or downloader might not work.")
+            logging.warning("Could not find ffmpeg executable, some VC features like the music player or downloader might not work.")
         jsruntime_path = os.getenv("JSRUNTIME")
         if not jsruntime_path or not os.path.exists(jsruntime_path):
-            logging.warning("Could not find javascript runtime binary, some vc features like the music player or downloader might not work.")
+            logging.warning("Could not find javascript runtime binary, some VC features like the music player or downloader might not work.")
+        
+        #check the trello API credentials & board ID
+        trellokey = os.getenv("TRELLOAPIKEY")
+        if trellokey == "" or trellokey == None:
+            logging.warning("Trello API Key is missing, some features may not work.")
+        trellosecret = os.getenv("TRELLOAPISECRET")
+        if trellosecret == "" or trellosecret == None:
+            logging.warning("Trello API Secret is missing, some features may not work.")
+        trellotoken = os.getenv("TRELLOTOKEN")
+        if trellotoken == "" or trellotoken == None:
+            logging.warning("Trello API Token is missing, some features may not work.")
+        trelloboard = os.getenv("BOARDID")
+        if trelloboard == "" or trelloboard == None:
+            logging.warning("Trello Board ID is missing, some features may not work.")
+        
+        #check the google API credentials
+        googleengineid = os.getenv("SEARCHENGINEID")
+        if googleengineid == "" or googleengineid == None:
+            logging.warning("Google search engine ID is missing, some features may not work.")
+        googleapikey = os.getenv("GOOGLEAPIKEY")
+        if googleapikey == "" or googleapikey == None:
+            logging.warning("Google search API Key is missing, some features may not work.")
+
+        # check to see if there are any TTS voices installed
+        
+        #check if any packages are out of date?
+        
+        
     
 
 # entrypoint
